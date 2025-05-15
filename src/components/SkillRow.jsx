fb490ad3358b155ef98d46051e1387fb0327ad4c
Robin <5983417+rorro@users.noreply.github.com>
2025-05-15 12:09:02 +0200

    Add Yama

--- src/components/SkillRow.jsx
+++ src/components/SkillRow.jsx
@@ -1,6 +1,15 @@
 import { getMetricName, formatNumber } from "@wise-old-man/utils";
 
 export default function SkillRow({ metric, top }) {
+  let metricName = metric.metric;
+  try {
+    metricName = getMetricName(metric.metric);
+  } catch (e) {
+    console.log(
+      `Error getting metric name for ${metric.metric}. Wise Old Man utils package probably needs to be updated.`
+    );
+  }
+
   return (
     <tr
       className={`border-b border-gray-700 text-sm text-gray-100 ${
@@ -10,9 +19,7 @@ export default function SkillRow({ metric, top }) {
       <td className="p-2">
         <img src={`metrics/${metric.metric}.png`} alt={metric.metric} />
       </td>
-      <td className="p-2 capitalize text-gray-300">
-        {getMetricName(metric.metric)}
-      </td>
+      <td className="p-2 capitalize text-gray-300">{metricName}</td>
       <td className="p-2 text-gray-300">
         {metric.rank === -1 ? "---" : formatNumber(metric.rank)}
       </td>
